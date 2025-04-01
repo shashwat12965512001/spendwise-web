b96af80d4c695566aed924ae5a2dd20d1c3c09de
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-04-01 17:09:27 +0530

    Refactor import paths in layout component for consistency

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import "./globals.css";
-import Header from "../Components/Header";
-import Footer from "../Components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export const metadata = {
   title: "Spendwise-Web",
