44f6046a27efe2a9e90f88a6405f6dcd51db3b96
shashwat12965512001 <ss12965512001@gmail.com>
2025-04-19 10:22:42 +0000

    Add user ID posting to FlutterBridge in LayoutWrapper on component mount

--- src/components/LayoutWrapper.jsx
+++ src/components/LayoutWrapper.jsx
@@ -2,8 +2,19 @@
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useEffect } from "react";
 
 export default function LayoutWrapper({ children }) {
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (user && user._id && window.FlutterBridge) {
+            window.FlutterBridge.postMessage(user._id);
+        } else {
+            console.log("FlutterBridge or user._id missing");
+        }
+    }, []);
+
+
     return (
         <>
             {/* Top bar for mobile / Header for desktop */}
