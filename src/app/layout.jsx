e37be28ffd8ec3e8cebfcd8dddd64918a67baf36
shashwat12965512001 <ss12965512001@gmail.com>
2025-04-19 09:05:16 +0000

    Add script to send user ID to FlutterBridge on page load

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,3 @@
-// NO "use client" here!
 import "./globals.css";
 import LayoutWrapper from "@/components/LayoutWrapper";
 
@@ -12,6 +11,16 @@ export default function RootLayout({ children }) {
     <html lang="en" className="hydrated">
       <body className="bg-black-100 text-gray-900 font-sans hydrated" data-new-gr-c-s-check-loaded="14.1231.0" data-gr-ext-installed="">
         <LayoutWrapper>{children}</LayoutWrapper>
+        <Script id="send-user-id-to-flutter" strategy="afterInteractive">
+          {`
+            window.onload = function () {
+              const user = JSON.parse(localStorage.getItem("user"));
+              if (user && user._id && window.FlutterBridge) {
+                window.FlutterBridge.postMessage(user._id);
+              }
+            };
+          `}
+        </Script>
       </body>
     </html>
   );
