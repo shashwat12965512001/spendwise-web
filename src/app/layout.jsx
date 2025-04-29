ff4581b506003d1e1c7fa9763d78c4bc216ac976
shashwat12965512001 <ss12965512001@gmail.com>
2025-04-29 11:03:33 +0000

    Update layout component to reflect new script loading state

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="hydrated">
-      <body className="bg-black-100 text-gray-900 font-sans hydrated" data-new-gr-c-s-check-loaded="14.1232.0" data-gr-ext-installed="">
+      <body className="bg-black-100 text-gray-900 font-sans hydrated" data-new-gr-c-s-check-loaded="14.1233.0" data-gr-ext-installed="">
         <LayoutWrapper>{children}</LayoutWrapper>
         <Script id="send-user-id-to-flutter" strategy="afterInteractive">
           {`
